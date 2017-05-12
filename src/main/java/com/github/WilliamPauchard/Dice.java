3f5ee878ef7b0d8a6a0ff891c5c8500acc6ce4da
William Pauchard <william.pauchard@hes.adhes.hesge.ch>
2017-05-12 10:09:57 +0200

    Premier jet d'un projet de lancé de dés

---
+++ src/main/java/com/github/WilliamPauchard/Dice.java
@@ -0,0 +1,36 @@
+package main.java.com.github.WilliamPauchard;
+/**
+ *
+ * @author william.pauchard
+ */
+public class Dice {
+
+    public static void main(String[] args) {
+        dice();
+    }
+
+    public static void dice() {
+        System.out.println("Le but est d'obtenir les chiffres suivants avec ces 3 dés : 4, 2, 1");
+
+        while (true) {
+            int dice1 = (int) (Math.random() * 6 + 1);
+            int dice2 = (int) (Math.random() * 6 + 1);
+            int dice3 = (int) (Math.random() * 6 + 1);
+
+            if (dice1 == 4 && dice2 == 2 && dice3 == 1) {
+                System.out.println("Vous avez gagné : \n Dé n°1 : " + dice1 + " \n Dé n°2 : " + dice2 + " \n Dé n°3 : " + dice3);
+                break;
+            } else if (dice1 == 1 && dice2 == 4 && dice3 == 2) {
+                System.out.println("Vous avez gagné : \n Dé n°1 : " + dice1 + " \n Dé n°2 : " + dice2 + " \n Dé n°3 : " + dice3);
+                break;
+            } else if (dice1 == 2 && dice2 == 1 && dice3 == 4) {
+                System.out.println("Vous avez gagné : \n Dé n°1 : " + dice1 + " \n Dé n°2 : " + dice2 + " \n Dé n°3 : " + dice3);
+                break;
+            } else {
+                System.out.println("Vous avez perdu : \n Dé n°1 : " + dice1 + " \n Dé n°2 : " + dice2 + " \n Dé n°3 : " + dice3);
+                break;
+            }
+
+        }
+    }
+}
